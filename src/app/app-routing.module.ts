05906523b87d2d0a6ca5bea7ed14ba9e07bd42d2
Ing_Chang_Tan <Ing.Chang.Tan@dell.com>
2019-09-04 13:44:27 +0800

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "main", component: MainComponent},
+  {path: "bitcoin", component: BitcoinComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BitcoinComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
